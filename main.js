91bbfcf9293a477b1cd670c1d61d4271dfd5bd10
parth05b <166864939+parth05b@users.noreply.github.com>
2024-04-13 00:53:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -325,7 +325,8 @@ function getAnskey(responsecontent) {
             "29/01/2024": mains29janshift1anskey,
             "30/01/2024": mains30janshift1anskey,
             "31/01/2024": mains31janshift1anskey,
-            "01/02/2024": mains1febshift1anskey
+            "01/02/2024": mains1febshift1anskey,
+            "09/04/2024": main9aprilshift1anskey
         } [date]
     }
 }
